e96c60543e5555e9cafe1d39e4c1f5593a99e4eb
Obafavour1 <olobafavour@gmail.com>
2024-11-12 09:05:05 +0000

    second

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import Header from './components/Header'
 import Services from './components/Services'
 import Roadmap from './components/Roadmap'
 import Testimonial from './components/Testimonial'
-import Team from './components/team'
-import Booking from './components/Booking'
+// import Team from './components/Team'
+// import Booking from './components/Booking'
 import Footer from './components/Footer'
 import Support from './components/Support'
+import Team from './components/Team'
 
 
 const App = () => {
